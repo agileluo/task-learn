4acd7cff053e25d1ce200b49e6ba10b10cb35008
agileluo <marlon.luo@qq.com>
2017-08-30 18:41:42 +0800

    init

---
+++ src/main/java/io/dandan/github/task/web/HelloController.java
@@ -0,0 +1,14 @@
+package io.dandan.github.task.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/test")
+	public @ResponseBody String sayHello(){
+		return "Hello Web";
+	}
+}
---
+++ src/main/java/io/dandan/github/task/web/TaskMemoryController.java
@@ -0,0 +1,14 @@
+package io.dandan.github.task.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 任务列表 - 内存实现
+ * @author Administrator
+ *
+ */
+@RestController
+public class TaskMemoryController {
+	
+	
+}
